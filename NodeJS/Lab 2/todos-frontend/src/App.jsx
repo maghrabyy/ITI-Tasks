f10657e766e858ae883462547503269f5e40abf0
Maghrabyy <mahmoud.elmaghraby11@gmail.com>
2024-07-14 19:16:20 +0300

    NodeJS Lab 2 Task -  Frontend - Spinner Loader update

--- NodeJS/Lab 2/todos-frontend/src/App.jsx
+++ NodeJS/Lab 2/todos-frontend/src/App.jsx
@@ -1,9 +1,11 @@
 import { useEffect } from "react";
 import { useState } from "react";
+import './App.css'
 
 function App() {
   const [ usersTodoList, setUsersTodoList ] = useState([])
   const [ todoList, setTodoList] = useState([])
+  const [ todoListLoader , setTodoListLoader ] = useState(false)
 
   useEffect(()=>{
     fetch("http://127.0.0.1:5050/todos").then(res=>res.json()).then(data=>{
@@ -16,7 +18,9 @@ function App() {
   const selectedUserId = userId=>{
     fetch(`http://127.0.0.1:5050/todos/${userId}`).then(res=>res.json()).then(data=>{
       setTodoList(data)
+      setTodoListLoader(false)
     }).catch(e=>console.log(e))
+    setTodoListLoader(true)
   }
 
   return (
@@ -30,13 +34,18 @@ function App() {
           </div>))}
       </div>
       <hr />
+      {todoListLoader? 
+      <div className="loader-container flex justify-center py-4">
+        <div className="loader"></div>
+      </div>
+      :
       <div className="todos-list grid md:grid-cols-3 grid-cols-2 gap-2 my-2 mx-1">
           {todoList.map(todo=>(
             <div className="todo-item p-2 bg-purple-800 text-white rounded-md" key={todo.id}>
               {todo.title}
             </div>
           ))}
-      </div>
+      </div>}
     </div>
   );
 }
